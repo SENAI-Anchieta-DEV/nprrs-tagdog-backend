8ac366ead861cf7f9c4647556e95224a7d216847
Raquel Yukie Tsuji <raqueltsuji@gmail.com>
2026-02-12 10:54:45 -0300

    NPRRST-48 chore: implementação inicial

---
+++ src/test/java/com/senai/nprrs_tagdog_backend/NprrsTagdogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.nprrs_tagdog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NprrsTagdogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
