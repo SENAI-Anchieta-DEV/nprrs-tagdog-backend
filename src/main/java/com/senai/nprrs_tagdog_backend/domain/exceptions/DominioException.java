085abce74d65da7736210f16d361028778f91f52
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2026-02-24 17:42:12 -0300

    feat: NPRRST-65 implementa exceções no pacote de domínio

---
+++ src/main/java/com/senai/nprrs_tagdog_backend/domain/exceptions/DominioException.java
@@ -0,0 +1,7 @@
+package com.senai.nprrs_tagdog_backend.domain.exceptions;
+
+public class OperacaoNaoPermitidaException extends RuntimeException {
+    public OperacaoNaoPermitidaException(String message) {
+        super(message);
+    }
+}
